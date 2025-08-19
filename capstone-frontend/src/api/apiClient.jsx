ec1c589387d04f0624d9aca1a32b1c6e635988df
HAMID <abdulhamidabari652@gmail.com>
2025-08-19 11:46:52 +0100

    Final front-end code

--- capstone-frontend/src/api/apiClient.jsx
+++ capstone-frontend/src/api/apiClient.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://final-project-y169.onrender.com/api';
-
+// const API_BASE_URL = 'https://final-project-y169.onrender.com/api';
+const API_BASE_URL = 'http://localhost:3000/api';
+ 
 const apiClient = axios.create({
     baseURL: API_BASE_URL,
     headers: {
